df5153168d78887d72f9552dbc454ee37fc2cca2
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2025-06-04 14:38:39 -0300

    Adding message convertor to RabbitMQConfig

--- msg-email/src/main/java/com/gabrielnz/msgemail/configs/RabbitMQConfig.java
+++ msg-email/src/main/java/com/gabrielnz/msgemail/configs/RabbitMQConfig.java
@@ -1,6 +1,8 @@
 package com.gabrielnz.msgemail.configs;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +15,10 @@ public class RabbitMQConfig {
     @Bean
     public Queue queue() {
         return new Queue(emailQueue, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter(new ObjectMapper());
     }
 }
