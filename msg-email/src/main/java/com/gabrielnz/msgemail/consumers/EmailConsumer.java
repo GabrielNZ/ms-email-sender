0e516d95e6e9f173bd25ab146e79ca6f68433c38
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2025-06-04 14:31:32 -0300

    EmailConsumer implemented

---
+++ msg-email/src/main/java/com/gabrielnz/msgemail/consumers/EmailConsumer.java
@@ -0,0 +1,14 @@
+package com.gabrielnz.msgemail.consumers;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+
+    @RabbitListener(queues = "${broker.queue.email.name}")
+    public void listEmailQueue(@Payload String email) {
+        System.out.println(email);
+    }
+}
